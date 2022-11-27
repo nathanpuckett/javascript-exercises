5bddfb03619a537c773ae86bda0ea11a6f476b03
nathanpuckett <nathanfpuckett@gmail.com>
2022-11-27 10:32:51 -0600

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+  const splitString = inputString.split("");
+  splitString.reverse();
+  let outputString = '';
+  for (const letter of splitString) {
+    outputString += letter;
+  }
+  return outputString;
 };
 
 // Do not edit below this line
