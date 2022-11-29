bf866f6b924db636faea1d0e202d3172ecf71ded
nathanpuckett <nathanfpuckett@gmail.com>
2022-11-29 17:55:42 -0600

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+  let sum = 0;
+  if (begin < 0 || end < 0 || typeof begin != 'number' || typeof end != 'number') {
+    return 'ERROR';
+  } else if (end > begin) {
+    for (let i = begin; i <= end; i++) {
+      sum += i;
+    }
+  
+  } else {
+    for (let i = end; i <= begin; i++) {
+      sum += i;
+    }
 
+  }
+  return sum;
 };
 
 // Do not edit below this line
