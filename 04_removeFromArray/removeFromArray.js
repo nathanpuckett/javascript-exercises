5fc2f594d63f3488a0c262fae06d42e3bec03910
nathanpuckett <nathanfpuckett@gmail.com>
2022-11-28 21:51:21 -0600

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...toRemove) {
+  for (let i = 0; i < inputArray.length; i++) {
+    for (let j = 0; j < toRemove.length; j++) {
+      if (inputArray[i] === toRemove[j]) {
+        inputArray.splice(i, 1);
+        i--;
+      }
+  
+    }
+  }
+  return inputArray;
 };
 
 // Do not edit below this line
