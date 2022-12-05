e18e792c19e4c1f7c09dee336c1f8f67237b6d0f
nathanpuckett <nathanfpuckett@gmail.com>
2022-12-04 19:32:59 -0600

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	let sum = 0;
+  for (const element of sumArray) {
+    sum += element;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(factors) {
+  let product = 1;
+  for (const factor of factors) {
+    product *= factor;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let factorial = 1;
+  for (let i = 1; i <= x; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
